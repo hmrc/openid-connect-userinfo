54437818832b7d3c5b46516774e7e20b3c507d81
Grzegorz Dobrowolski <grzegorz.dobrowolski@digital.hmrc.gov.uk>
2017-06-19 11:17:44 +0100

    PE-2879 Downgrade libs
    Upgrade of libs will bo done as a separate task.

--- project/MicroserviceBuild.scala
+++ project/MicroserviceBuild.scala
@@ -21,11 +21,11 @@ private object AppDependencies {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "microservice-bootstrap" % "5.14.0",
-    "uk.gov.hmrc" %% "play-authorisation" % "4.3.0",
-    "uk.gov.hmrc" %% "play-config" % "4.3.0",
+    "uk.gov.hmrc" %% "microservice-bootstrap" % "5.8.0",
+    "uk.gov.hmrc" %% "play-authorisation" % "4.2.0",
+    "uk.gov.hmrc" %% "play-config" % "3.0.0",
     "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
-    "uk.gov.hmrc" %% "play-health" % "2.1.0",
+    "uk.gov.hmrc" %% "play-health" % "2.0.0",
     "org.scalacheck" %% "scalacheck" % "1.12.5",
     "uk.gov.hmrc" %% "play-hmrc-api" % "1.2.0",
     "uk.gov.hmrc" %% "domain" % "4.0.0"
