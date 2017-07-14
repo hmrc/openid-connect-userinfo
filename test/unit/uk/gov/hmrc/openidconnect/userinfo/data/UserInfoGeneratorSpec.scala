e9c613c9b42fce069b894e0bacfada0542380dcf
Allen Fensome <allen.fensome@gmail.com>
2017-07-14 12:18:36 +0100

    PE-3136 Adding feature flag functionality and a feature flag for the 2 digit country code, defaulting to off.

--- test/unit/uk/gov/hmrc/openidconnect/userinfo/data/UserInfoGeneratorSpec.scala
+++ test/unit/uk/gov/hmrc/openidconnect/userinfo/data/UserInfoGeneratorSpec.scala
@@ -19,24 +19,40 @@ package unit.uk.gov.hmrc.openidconnect.userinfo.data
 import uk.gov.hmrc.openidconnect.userinfo.data.UserInfoGenerator
 import uk.gov.hmrc.openidconnect.userinfo.domain.UserInfo
 import org.joda.time.LocalDate
+import org.scalatest.BeforeAndAfterEach
 import org.scalatest.prop.PropertyChecks
+import uk.gov.hmrc.openidconnect.userinfo.config.{FeatureSwitch, UserInfoFeatureSwitches}
 import uk.gov.hmrc.play.test.UnitSpec
 
-class UserInfoGeneratorSpec extends UnitSpec with PropertyChecks {
+class UserInfoGeneratorSpec extends UnitSpec with PropertyChecks with BeforeAndAfterEach {
   val ninoPattern = "^[A-CEGHJ-NOPR-TW-Z]{2}[0-9]{6}[ABCD\\s]{1}$".r
   val from = new LocalDate(1939, 12, 27)
   val until = new LocalDate(1998, 12, 29)
+
+
+  override protected def beforeEach(): Unit = {
+      FeatureSwitch.enable(UserInfoFeatureSwitches.countryCode)
+  }
 
   "userInfo" should {
     "generate an OpenID Connect compliant UserInfo response" in forAll(UserInfoGenerator.userInfo) { userInfo: UserInfo =>
       UserInfoGenerator.firstNames should contain (userInfo.given_name)
       UserInfoGenerator.middleNames should contain (userInfo.middle_name)
       UserInfoGenerator.lastNames should contain (userInfo.family_name)
-      userInfo.address shouldBe UserInfoGenerator.address
+      userInfo.address shouldBe UserInfoGenerator.addressWithCountryCode
       assertValidDob(userInfo.birthdate.getOrElse(fail(s"Generated user's dob is not defined")))
       assertValidNino(userInfo.uk_gov_nino.getOrElse(fail(s"Generated user's NINO is not defined")))
     }
   }
+
+  "userInfo" should {
+    "generate an OpenID Connect compliant UserInfo response without country code when feature flag is disabled" in forAll( {
+      FeatureSwitch.disable(UserInfoFeatureSwitches.countryCode)
+      UserInfoGenerator.userInfo } ) { userInfo: UserInfo =>
+      userInfo.address shouldBe UserInfoGenerator.addressWithoutCountryCode
+    }
+  }
+
 
   private def assertValid(name: String, expected: List[String], actual: Option[String]): Unit = {
     expected should contain (actual.getOrElse(fail(s"Generated user's $name is not defined")))
