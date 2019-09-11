386b10086a89ff3f423dd159ba3d87f4e03aafa1
yuan zhang <yuanzhang910@hotmail.com>
2019-09-11 16:00:23 +0100

    AIV-1276: change api status

--- project/MicroserviceBuild.scala
+++ project/MicroserviceBuild.scala
@@ -14,24 +14,24 @@ private object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % "4.14.0",
-    "org.scalacheck" %% "scalacheck" % "1.13.5",
-    "uk.gov.hmrc" %% "play-hmrc-api" % "3.4.0-play-25",
-    "uk.gov.hmrc" %% "domain" % "5.6.0-play-25",
-    "uk.gov.hmrc" %% "auth-client" % "2.27.0-play-25"
+    "org.scalacheck" %% "scalacheck"        % "1.13.5",
+    "uk.gov.hmrc"    %% "bootstrap-play-25" % "5.0.0",
+    "uk.gov.hmrc"    %% "play-hmrc-api"     % "3.6.0-play-25",
+    "uk.gov.hmrc"    %% "domain"            % "5.6.0-play-25",
+    "uk.gov.hmrc"    %% "auth-client"       % "2.28.0-play-25"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "service-integration-test" % "0.9.0-play-25" % testScope,
-    "org.scalatestplus.play" %% "scalatestplus-play" % "2.0.1" % testScope,
-    "org.scoverage" %% "scalac-scoverage-plugin" % "1.3.1" % testScope,
-    "org.pegdown" % "pegdown" % "1.6.0" % testScope,
-    "org.jsoup" % "jsoup" % "1.10.2" % testScope,
-    "com.typesafe.play" %% "play-test" % PlayVersion.current % testScope,
-    "com.github.tomakehurst" % "wiremock" % "1.58" % testScope,
-    "org.mockito" % "mockito-all" % "1.10.19" % testScope,
-    "org.scalaj" %% "scalaj-http" % "2.3.0" % testScope,
-    "com.github.fge" % "json-schema-validator" % "2.2.6" % testScope
+    "com.typesafe.play"      %% "play-test"                % PlayVersion.current % testScope,
+    "uk.gov.hmrc"            %% "service-integration-test" % "0.9.0-play-25"     % testScope,
+    "org.scalatestplus.play" %% "scalatestplus-play"       % "2.0.1"             % testScope,
+    "org.scoverage"          %% "scalac-scoverage-plugin"  % "1.3.1"             % testScope,
+    "org.pegdown"             % "pegdown"                  % "1.6.0"             % testScope,
+    "org.jsoup"               % "jsoup"                    % "1.10.2"            % testScope,
+    "com.github.tomakehurst"  % "wiremock"                 % "1.58"              % testScope,
+    "org.mockito"             % "mockito-all"              % "1.10.19"           % testScope,
+    "org.scalaj"             %% "scalaj-http"              % "2.3.0"             % testScope,
+    "com.github.fge"          % "json-schema-validator"    % "2.2.6"             % testScope
   )
 
   def apply(): Seq[ModuleID] = compile ++ test
